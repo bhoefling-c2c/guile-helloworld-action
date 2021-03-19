750ef4ed0eba059966bae45cb43193408d934d61
Björn Höfling <bjoern.hoefling@camptocamp.com>
2021-03-19 12:18:23 +0100

    Factor out debug/warning message.

--- hello.scm
+++ hello.scm
@@ -3,12 +3,24 @@
 	 (name (or (getenv env-name) "World")))
     name))
 
+;; See for more actions:
+;; https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+
 (define (write-output var value)
   "Writes the content of an output variable to the log"
   (format #t "::set-output name=~a::~a~%" var value))
 
+(define (debug-msg msg)
+  "Outputs a debug message. In order to see these,
+a secret ACTIONS_STEP_DEBUG=true has to be defined in your repo."
+  (format #t "::debug::~a~%" msg))
+
+(define (simple-warning msg)
+;; ::warning file={name},line={line},col={col}::{message}
+(format #t "::warning ::~a~%" msg))
+
 (let ((greeting (string-append "Hello, " name "!")))
-  (format #t "::debug::~a~%" "This is a debug message")
+  (debug-msg "This is a debug message")
+  (simple-warning "This is a warning without file context")
   (format #t "~a~%" greeting)
   (write-output "full-greeting" greeting))
-
